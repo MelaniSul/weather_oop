11c7d71c78423850fee6b3d9badd51abefbafa5d
MelaniSul <melani.sulengo@khk.ee>
2020-12-15 23:03:19 +0200

    Weather data is red

---
+++ weather.js
@@ -0,0 +1,13 @@
+class Weather{
+    constructor(city) {
+        this.city = city;
+        this.key = '77b9c09f4440335ae3418f0aa5fff2d8';
+    }
+
+    //data from api
+    async weatherData(){
+        const resp = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + this.city + '&appid' + this.key);
+        const data = await resp.json();
+        return data;
+    }
+}
